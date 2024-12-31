3b0d20862df23f0b5240dc1358a1e8c0fb4045c0
Max T <dev@maxthakur.xyz>
2024-12-30 21:39:45 -0500

    Hopefully this is the root of all our problems

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
+import image from "@astrojs/image";
+import prefetch from "@astrojs/prefetch";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
@@ -9,15 +11,13 @@ import compress from "astro-compress";
 
 // https://astro.build/config
 
-// https://astro.build/config
-import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://sirius.menu/",
-  integrations: [sitemap(), tailwind({
+  integrations: [image(), prefetch(), sitemap(), tailwind({
     config: {
       applyBaseStyles: false
     }
-  }), compress(), partytown()]
+  }), compress()]
 });
